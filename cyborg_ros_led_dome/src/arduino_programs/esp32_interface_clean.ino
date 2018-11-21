506ca573f907c28c60cd61b55986305720ad6892
aregb <aregb@stud.ntnu.no>
2018-11-21 12:28:08 +0100

    add esp32-interface-clean

---
+++ cyborg_ros_led_dome/src/arduino_programs/esp32_interface_clean.ino
@@ -0,0 +1,68 @@
+#include "FastLED.h"
+#define NUM_LEDS 791
+#define DATA_PIN 33
+#define BAUDRATE 1000000
+
+
+// BUFFER AND VARIABLES FOR RECEIVING A BYTEARRAY
+uint8_t buffer[NUM_LEDS * 3]; // Each led has 3 bytes of data (One for each color value)
+boolean gotData = false;   // Got all data we needed to set leds
+
+// INITIALIZE LEDS
+CRGB leds[NUM_LEDS];
+
+
+// SETUP LEDS AND SERIAL COMMUINICATION
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.clear();
+  FastLED.show();
+  Serial.begin(BAUDRATE, SERIAL_8N1);     //Starting serial communication, 8 data bits, no parity, 1 stop bit
+}
+
+void loop() {
+  receiveSerialData();
+  setLedsFromBuffer(); 
+}
+
+
+void receiveSerialData() {
+  static boolean recvInProgress = false;
+  static int index =0;
+  uint8_t startMarker = 254;
+  uint8_t endMarker = 255;
+  uint8_t rb;
+  
+  while (Serial.available() > 0 && gotData==false) {
+    rb = Serial.read();
+
+    if (recvInProgress==true) {
+      if (rb != endMarker) {
+        buffer[index] = rb;
+        index++;
+      }
+      else {
+        recvInProgress = false;
+        index = 0;
+        gotData = true;
+      }
+    }
+    else if (rb == startMarker) {
+      recvInProgress = true;
+    }
+  }
+}
+
+
+// SETTING LEDS FROM RECEIVED DATA
+void setLedsFromBuffer(){
+ if(gotData == true){
+    for (int i=0; i<NUM_LEDS*3; i+=3){
+        leds[i/3].r = buffer[i];
+        leds[i/3].g = buffer[i+1];
+        leds[i/3].b = buffer[i+2];
+    }
+    FastLED.show();
+    gotData = false;
+ }
+}
